dc2b0e61085d2c29588112f8230fcbc686a846a4
Adriano <eama@cin.ufpe.br>
2014-08-27 12:45:19 -0300

    Comitando o projeto.

---
+++ Criptografia/src/adriano/com/Main.java
@@ -0,0 +1,30 @@
+package adriano.com;
+
+public class Main {
+
+	static String plaintext = "Vamos atacá-los hoje a noite"; /* Note null padding */
+
+	public static void main(String[] args) {
+		try {
+
+			Criptografia cripto = new Criptografia();
+			
+			System.out.println("plain:   " + plaintext);
+
+			byte[] cipher = cripto.encrypt(plaintext);
+
+			System.out.print("cipher:  ");
+			for (int i = 0; i < cipher.length; i++)
+				System.out.print(new Integer(cipher[i]) + " ");
+			System.out.println("");
+
+			String decrypted = cripto.decrypt(cipher);
+
+			System.out.println("decrypt: " + decrypted);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
